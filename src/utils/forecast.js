a7918f42c25e7b66fd11a8319b219900aab01325
Shikhar-SRJ <ptksuraj@gmail.com>
2021-01-26 21:51:43 +0530

    Humidity data added.

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback(body.error.info, undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0]+'. Its currently '+body.current.temperature+' degrees out. It feels like '+body.current.feelslike+' degrees out')
+            callback(undefined, body.current.weather_descriptions[0]+'.\n Its currently '+body.current.temperature+' degrees out. It feels like '+body.current.feelslike+' degrees out.\n Humidity is '+body.current.humidity)
         }
     })
 }
